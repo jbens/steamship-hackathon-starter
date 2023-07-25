7e12e045fbda0b2fa6388987087166c76b60750b
jbens <jchevisbenson@gmail.com>
2023-07-25 01:01:52 +0000

    testing with hackathon code

--- src/api.py
+++ src/api.py
@@ -1,14 +1,39 @@
-"""Minimum viable AgentService implementation.
+from typing import List
 
-This will result in an agent that effectively acts like ChatGPT.
-"""
+from langchain.agents import Tool
 
-from steamship.agents.functional import FunctionsBasedAgent
-from steamship.agents.llms.openai import ChatOpenAI
-from steamship.agents.service.agent_service import AgentService
+# noinspection PyUnresolvedReferences
+from core.agent.agent import Agent
+from core.agent.base import BaseAgent
+
+# noinspection PyUnresolvedReferences
+from core.agent.chat_agent import ChatAgent
+
+# noinspection PyUnresolvedReferences
+from tools import SearchTool, GenerateImageTool, MyTool
 
 
-class MyAssistant(AgentService):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self._agent = FunctionsBasedAgent(llm=ChatOpenAI(self.client), tools=[])
+class MyAgent(Agent, BaseAgent):
+    """
+    If you want your agent to be tool-based, use the following line:
+        class MyAgent(Agent, BaseAgent)
+
+    If you want your agent to be chatty and personality-based, use the following line:
+        class MyAgent(ChatAgent, BaseAgent)
+    """
+
+    def is_verbose_logging_enabled(self) -> bool:
+        return True
+
+    def get_tools(self) -> List[Tool]:
+        return [
+            # SearchTool(self.client),
+             MyTool(self.client),
+            # GenerateImageTool(self.client),
+        ]
+
+    def get_personality(self) -> str:
+        """Return a string that completes this sentence. The agent acts like..."""
+        return """
+        a world-class horticulturalist. You are particularly skilled at identifying which plants would thrive in a given geographic location. 
+        """
